0d17e702251a2648b6dac321b98f11e49d31e12a
AGMBK <agmbk.github@gmail.com>
2023-01-15 20:40:57 +0100

    User display

--- SaeA01/Framework/Symfony/src/Controller/Home.php
+++ SaeA01/Framework/Symfony/src/Controller/Home.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\EquipesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,11 +12,10 @@ class Home extends AbstractController
     #[Route('/', name: '', methods: ['GET'])]
     #[Route('/index', name: 'index', methods: ['GET'])]
     #[Route('/home', name: 'home', methods: ['GET'])]
-    public function index(EquipesRepository $equipesRepository): Response
+    public function index(): Response
     {
-        var_dump(get_current_user());
         return $this->render('home/index.html.twig', [
-            'controller_name' => '',
+            'username' => $this->getUser()->getUserIdentifier(),
         ]);
     }
 }
