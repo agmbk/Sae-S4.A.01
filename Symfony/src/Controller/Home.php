2fb4a761e07caf9c87b58777af3eed69bb1cfffd
gombouka <arnaudgg26@gmail.com>
2023-03-07 14:14:43 +0100

    refactor(Symfony):

---
+++ Symfony/src/Controller/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class Home extends AbstractController
+{
+    #[Route('/', name: '', methods: ['GET'])]
+    #[Route('/index', name: 'index', methods: ['GET'])]
+    #[Route('/home', name: 'home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home/home.html.twig', [
+            //'username' => $this->getUser()->getUserIdentifier(),
+        ]);
+    }
+}
